b6f6ef7ff3307d35dcca9f12a5dce29ed7429109
Karanveer Singh <singhkaranveer671@gmail.com>
2019-01-25 18:34:04 -0800

    initial cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 // renders app component into the div id root in index.html
 ReactDOM.render(<App />, document.getElementById('root'));
